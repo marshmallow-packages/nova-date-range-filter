16fcc289df654d718d71935c441dae27967b166f
Martin Petrov <martinpetrov@Martins-MacBook-Air.local>
2021-03-25 10:57:28 +0200

    upd: comments from review

--- src/DateRangeFilter.php
+++ src/DateRangeFilter.php
@@ -8,23 +8,22 @@ abstract class DateRangeFilter extends Filter
 {
     public $component = 'date-range-filter';
 
+
     public function enableTime()
     {
-        return $this->withMeta(['enableTime' => true]);
-    }
-
-    public function modeFormat()
-    {
-        return $this->withMeta(['mode' => $this->mode]);
+        $this->withMeta(['enableTime' => true]);
+        return $this;
     }
 
     public function dateFormat($format)
     {
-        return $this->withMeta(['dateFormat' => $format]);
+        $this->withMeta(['dateFormat' => $format]);
+        return $this;
     }
 
     public function placeholder($placeholder)
     {
-        return $this->withMeta(['placeholder' => $placeholder]);
+        $this->withMeta(['placeholder' => $placeholder]);
+        return $this;
     }
 }
