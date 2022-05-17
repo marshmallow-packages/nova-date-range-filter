311e2ed60fe07e172ce56dd7e422402e3245c21a
LTKort <lars.kort94@gmail.com>
2022-05-17 18:01:38 +0200

    add Support for Nova4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,17 @@
 let mix = require("laravel-mix");
+let path = require("path");
+
+require("./mix");
 
 mix.setPublicPath("dist")
-    .js("resources/js", "js/nova-date-range-filter.js")
-    .vue();
+    .js("resources/js/index.js", "js/nova-date-range-filter.js")
+    .vue({ version: 3 })
+    .nova("marshmallow/nova-date-range-filter");
+
+mix.alias({
+    "laravel-nova": path.join(
+        __dirname,
+        "vendor/laravel/nova/resources/js/mixins/packages.js"
+    ),
+    "@": path.join(__dirname, "resources/js/"),
+});
